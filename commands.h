9551c4236f13579f3011c5abfa211b9e401c691d
Kaworu <kaworu@k2t.eu>
2016-02-27 11:41:02 +0100

    Separated configuration and protocol definitions

---
+++ commands.h
@@ -0,0 +1,32 @@
+#pragma once
+
+/* SLA+R */
+#define CMD_WAIT		0x00
+#define CMD_READ_VERSION	0x01
+#define CMD_READ_MEMORY		0x02
+/* internal mappings */
+#define CMD_READ_CHIPINFO	(0x10 | CMD_READ_MEMORY)
+#define CMD_READ_FLASH		(0x20 | CMD_READ_MEMORY)
+#define CMD_READ_EEPROM		(0x30 | CMD_READ_MEMORY)
+#define CMD_READ_PARAMETERS	(0x40 | CMD_READ_MEMORY)	/* only in APP */
+
+/* SLA+W */
+#define CMD_SWITCH_APPLICATION	CMD_READ_VERSION
+#define CMD_WRITE_MEMORY	CMD_READ_MEMORY
+/* internal mappings */
+#define CMD_BOOT_BOOTLOADER	(0x10 | CMD_SWITCH_APPLICATION)	/* only in APP */
+#define CMD_BOOT_APPLICATION	(0x20 | CMD_SWITCH_APPLICATION)
+#define CMD_WRITE_CHIPINFO	(0x10 | CMD_WRITE_MEMORY)	/* invalid */
+#define CMD_WRITE_FLASH		(0x20 | CMD_WRITE_MEMORY)
+#define CMD_WRITE_EEPROM	(0x30 | CMD_WRITE_MEMORY)
+#define CMD_WRITE_PARAMETERS	(0x40 | CMD_WRITE_MEMORY)	/* only in APP */
+
+/* CMD_SWITCH_APPLICATION parameter */
+#define BOOTTYPE_BOOTLOADER	0x00				/* only in APP */
+#define BOOTTYPE_APPLICATION	0x80
+
+/* CMD_{READ|WRITE}_* parameter */
+#define MEMTYPE_CHIPINFO	0x00
+#define MEMTYPE_FLASH		0x01
+#define MEMTYPE_EEPROM		0x02
+#define MEMTYPE_PARAMETERS	0x03				/* only in APP */
---
+++ config.h
@@ -0,0 +1,92 @@
+/***************************************************************************
+ *   Copyright (C) 2016 Tomek Nagisa, Kaworu                               *
+ *   kaworu@k2t.eu                                                         *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; version 2 of the License,               *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#pragma once
+
+/*
+ * atmega8:
+ * Fuse H: 0xda (512 words bootloader)
+ * Fuse L: 0x84 (8Mhz internal RC-Osz., 2.7V BOD)
+ *
+ * atmega88:
+ * Fuse E: 0xfa (512 words bootloader)
+ * Fuse H: 0xdd (2.7V BOD)
+ * Fuse L: 0xc2 (8Mhz internal RC-Osz.)
+ *
+ * atmega168:
+ * Fuse E: 0xfa (512 words bootloader)
+ * Fuse H: 0xdd (2.7V BOD)
+ * Fuse L: 0xc2 (8Mhz internal RC-Osz.)
+ *
+ * atmega328p:
+ * Fuse E: 0xfd (2.7V BOD)
+ * Fuse H: 0xdc (512 words bootloader)
+ * Fuse L: 0xc2 (8Mhz internal RC-Osz.)
+ */
+
+#if defined (__AVR_ATmega8__)
+#define VERSION_STRING		"TWIBOOT m8v2.1"
+#define SIGNATURE_BYTES		0x1E, 0x93, 0x07
+
+#elif defined (__AVR_ATmega88__)
+#define VERSION_STRING		"TWIBOOT m88v2.1"
+#define SIGNATURE_BYTES		0x1E, 0x93, 0x0A
+
+#elif defined (__AVR_ATmega168__)
+#define VERSION_STRING		"TWIBOOT m168v2.1"
+#define SIGNATURE_BYTES		0x1E, 0x94, 0x06
+
+#elif defined (__AVR_ATmega328P__)
+#define VERSION_STRING		"TWIBOOTm328pv2.1"
+#define SIGNATURE_BYTES		0x1E, 0x95, 0x0F
+
+#else
+#error MCU not supported
+#endif
+
+#define EEPROM_SUPPORT		1
+#define LED_SUPPORT		1
+
+/* 25ms @8MHz */
+#define TIMER_RELOAD		(0xFF - 195)
+
+/* 40 * 25ms */
+#define TIMEOUT			40
+
+#if LED_SUPPORT
+#define LED_INIT()		DDRB = ((1<<PORTB4) | (1<<PORTB5))
+#define LED_RT_ON()		PORTB |= (1<<PORTB4)
+#define LED_RT_OFF()		PORTB &= ~(1<<PORTB4)
+#define LED_GN_ON()		PORTB |= (1<<PORTB5)
+#define LED_GN_OFF()		PORTB &= ~(1<<PORTB5)
+#define LED_GN_TOGGLE()		PORTB ^= (1<<PORTB5)
+#define LED_OFF()		PORTB = 0x00
+#else
+#define LED_INIT()
+#define LED_RT_ON()
+#define LED_RT_OFF()
+#define LED_GN_ON()
+#define LED_GN_OFF()
+#define LED_GN_TOGGLE()
+#define LED_OFF()
+#endif
+
+#ifndef TWI_ADDRESS
+#define TWI_ADDRESS		0x29
+#endif
